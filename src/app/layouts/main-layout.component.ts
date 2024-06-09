9afc83485a22c5ca987e9859fe66a6eff2769846
Manh4real <nguyenvanmanh30102001dhcn@gmail.com>
2024-06-09 16:12:22 +0700

    amend

--- src/app/layouts/main-layout.component.ts
+++ src/app/layouts/main-layout.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../services/auth.service';
       <div
         class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
       >
-        <div class="w-full md:block md:w-auto" id="navbar-default">
+        <div class="!w-full md:block md:w-auto" id="navbar-default">
           <ul
             class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
           >
@@ -32,15 +32,8 @@ import { AuthService } from '../services/auth.service';
                 Cart ({{ (cartService.cart$ | async)?.length }})
               </a>
             </li>
-            <li *ngIf="authService.getToken()">
-              <a
-                routerLink="/shopping-cart"
-                routerLinkActive="!text-blue-500"
-                [routerLinkActiveOptions]="{ exact: true }"
-                class="block text-white"
-              >
-                Cart ({{ (cartService.cart$ | async)?.length }})
-              </a>
+            <li class="!ml-auto" *ngIf="!authService.getToken()">
+              <a routerLink="/login" class="block text-white"> Log in </a>
             </li>
           </ul>
         </div>
